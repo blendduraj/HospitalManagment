d066858671b39cb9f6135a15b0c0f6d6a2868c53
Blendduraj <blend.duraj01@gmail.com>
2024-01-02 23:05:39 +0100

    My first commit

---
+++ HospitalManagment1/src/Classes/Doctor.java
@@ -0,0 +1,63 @@
+package Classes;
+import java.util.*;
+
+public class Doctor extends Appointments implements myfunction
+{
+
+    List<Object[]> patlist = Patient.patlist;
+    Scanner sc= new Scanner(System.in);
+    Appointments a = new Appointments();
+    public void checkApp()
+    {
+        System.out.println("Enter ur ID");
+        String sd = sc.next();
+
+        for(Object[] o : patlist)
+        {
+
+            if(sd.equals(o[3]))
+            {
+                //System.out.println("Found");
+                System.out.print(o[0] + "\t");
+                System.out.print(o[1] + "\t");
+                System.out.print(o[2] + "\t");
+                System.out.print(o[3] + "\n");
+
+            }
+
+        }
+
+    }
+    public void remEnt()
+    {
+        checkApp();
+        System.out.println("Enter id of Patient who finished ur treatment");
+        String  rd = sc.next();
+        for(Object[] o : patlist)
+        {
+
+            if(rd.equals(o[0]))
+            {
+                System.out.println("Patient removed Successfully");
+                patlist.remove(o);
+                break;
+
+            }
+
+        }
+    }
+    public void showDocs()
+    {
+        Admin a = new Admin();
+        List<Object[]> doclist = Admin.doclist;
+        System.out.println("Avaialable Doctors\n");
+        for(Object[] o : doclist)
+        {
+            if(o[3]=="1")
+            {
+                System.out.println("Doc id = "+o[1]+" Doc Name = "+o[2]);
+            }
+        }
+    }
+
+}
